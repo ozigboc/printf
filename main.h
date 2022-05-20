c06e7609d00e2acba54f22df99e11a93cd48dc86
ozigboc <ozigboc@gmail.com>
2022-05-20 11:16:26 +0100

    team task

--- main.h
+++ main.h
@@ -5,29 +5,29 @@
 #include <stdarg.h>
 
 /**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
+ * * struct flags - struct containing flags to "turn on"
+ * * when a flag specifier is passed to _printf()
+ * * @plus: flag for the '+' character
+ * * @space: flag for the ' ' character
+ * * @hash: flag for the '#' character
  */
 typedef struct flags
 {
-    int plus;
-    int space;
-    int hash;
+	int plus;
+	int space;
+	int hash;
 } flags_t;
 
 /**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
+ * * struct printHandler - struct to choose the right function depending
+ * * on the format specifier passed to _printf()
+ * * @c: format specifier
+ * * @f: pointer to the correct printing function
  */
 typedef struct printHandler
 {
-    char c;
-    int (*f)(va_list ap, flags_t *f);
+	char c;
+	int (*f)(va_list ap, flags_t *f);
 } ph;
 
 /* print_nums */
@@ -72,6 +72,5 @@ int print_address(va_list l, flags_t *f);
 
 /* print_percent */
 int print_percent(va_list l, flags_t *f);
-
 
 #endif
