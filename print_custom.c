21d494a9f73318d68e4c2f9d66392383da50dd6e
Muyiwaa <enochmwilliams@gmail.com>
2022-05-21 12:18:28 -0700

    corrected

--- print_custom.c
+++ print_custom.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+ /**
  * print_bigS - Non printable characters
  * (0 < ASCII value < 32 or >= 127) are
  * printed this way: \x, followed by the ASCII code
@@ -10,6 +10,7 @@
  * if a flag is passed to _printf
  * Return: number of char printed
  */
+
 int print_bigS(va_list l, flags_t *f)
 {
 	int i, count = 0;
@@ -37,13 +38,14 @@ int print_bigS(va_list l, flags_t *f)
 	return (count);
 }
 
-/**
+ /**
  * print_rev - prints a string in reverse
  * @l: argument from _printf
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
  * Return: length of the printed string
  */
+
 int print_rev(va_list l, flags_t *f)
 {
 	int i = 0, j;
@@ -52,7 +54,6 @@ int print_rev(va_list l, flags_t *f)
 	(void)f;
 	if (!s)
 		s = "(null)";
-
 	while (s[i])
 		i++;
 
@@ -62,13 +63,14 @@ int print_rev(va_list l, flags_t *f)
 	return (i);
 }
 
-/**
+ /**
  * print_rot13 - prints a string using rot13
  * @l: list of arguments from _printf
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
  * Return: length of the printed string
  */
+
 int print_rot13(va_list l, flags_t *f)
 {
 	int i, j;
@@ -80,7 +82,9 @@ int print_rot13(va_list l, flags_t *f)
 	for (j = 0; s[j]; j++)
 	{
 		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+		{
 			_putchar(s[j]);
+		}
 		else
 		{
 			for (i = 0; i <= 52; i++)
@@ -90,16 +94,16 @@ int print_rot13(va_list l, flags_t *f)
 			}
 		}
 	}
-
 	return (j);
 }
 
-/**
+ /**
  * print_percent - prints a percent
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags in which we turn the flags on
  * Return: number of char printed
  */
+
 int print_percent(va_list l, flags_t *f)
 {
 	(void)l;
