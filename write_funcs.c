949c8174a7f67b2cdb3a33e398ff869eb925e06f
Muyiwaa <enochmwilliams@gmail.com>
2022-05-21 12:14:12 -0700

    corrected

--- write_funcs.c
+++ write_funcs.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <unistd.h>
 
-<<<<<<< HEAD
  /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,15 +8,6 @@
  * On error, -1 is returned, and errno is set appropriately.
  * Description: _putchar uses a local buffer of 1024 to call write
  * as little as possible
-=======
-/**
- * * _putchar - writes the character c to stdout
- * * @c: The character to print
- * * Return: On success 1.
- * * On error, -1 is returned, and errno is set appropriately.
- * * Description: _putchar uses a local buffer of 1024 to call write
- * * as little as possible
->>>>>>> ec33472a1e2943f75b186537b36845ab35bc3be3
  */
 
 int _putchar(char c)
@@ -38,17 +28,10 @@ int _putchar(char c)
 	return (1);
 }
 
-<<<<<<< HEAD
  /**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
  * Return: number of chars written
-=======
-/**
- * * _puts - prints a string to stdout
- * * @str: pointer to the string to print
- * * Return: number of chars written
->>>>>>> ec33472a1e2943f75b186537b36845ab35bc3be3
  */
 
 int _puts(char *str)
